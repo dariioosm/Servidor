f7fb2111e8de3df694254f1fd6e70d0b7e5bf2e0
Dario Sanchez <dariosanchez351@gmail.com>
2024-09-23 14:26:47 +0200

    comienzo del segundo ejercicio

---
+++ tema1/array/array2.php
@@ -0,0 +1,11 @@
+<?php 
+/* 
+caragar vector con valores 10 20 30 40 50 mediante bucle y que haga promedio del contenido del vector
+
+*/
+
+for($i=0;$i<=5;$i++){
+$M[$i]=$i*10;
+}var_dump($M);
+
+?>
