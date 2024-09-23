7af09a7464c1207d668f14c28b2d8217c89506d8
dariioosm <dariosanchez351@gmail.com>
2024-09-23 22:00:25 +0200

    planteamiento de ejercicios 5 y 6

---
+++ p1unidad2/ejercicio5.php
@@ -0,0 +1,11 @@
+<?php 
+/*
+*Crear un programa partir de 3 valores, a b y c que corresponden a los tres
+*coeficientes de una ecuación de segundo grado muestre las soluciones de la
+*misma La solución de la ecuación de segundo grado depende del signo de b2-4ac:
+* si b2-4ac es negativo no hay soluciones
+* si es nulo, hay sólo una solución -b/2a
+* si es positivo, hay dos soluciones: (-b+sqrt(b2-4ac))/(2a) y (-bsqrt(b2-4ac))/(2a)
+
+*/
+?>
---
+++ p1unidad2/ejercicio6.php
@@ -0,0 +1,10 @@
+<?php 
+/*
+*Crear un programa con bucle for para contar desde 0 a 50 de 5 en 
+*/
+$contador=0;
+for ($i=0;$i<=50;$i+5){
+    $contador=$i++;
+    echo $contador.'<br>';
+}
+?>
