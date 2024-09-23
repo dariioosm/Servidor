e6a8965541490e41b1bf62c5c3fd532899582e38
dariioosm <dariosanchez351@gmail.com>
2024-09-23 18:54:11 +0200

    cambiar simbolo < por >

--- p1unidad2/ejercicio2.php
+++ p1unidad2/ejercicio2.php
@@ -7,14 +7,14 @@
 $numero1=2;
 $numero2=3;
 $numero3=4;
-if($numero1<$numero2&&$numero1<$numero3){
-    echo 'el numero mayor es: '+$numero1;
-}
-if($numero2<$numero1&&$numero2<$numero3){
-    echo 'el numero mayor es: '+$numero2;
-}
+if($numero1>$numero2&&$numero1>$numero3){
+    echo "el numero mayor es:" .$numero1;
+}else
+if($numero2>$numero1&&$numero2>$numero3){
+    echo "el numero mayor es:". $numero2;
+}else
 
-if($numero3<$numero1&&$numero3<$numero2){
-    echo 'el numero mayor es: '+$numero3;
+if($numero3>$numero1&&$numero3>$numero2){
+    echo "el numero mayor es:".$numero3;
 }
 ?>
