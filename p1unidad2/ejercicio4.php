9d68a137ef49bca582ab95829f8d1439823cbe14
dariioosm <dariosanchez351@gmail.com>
2024-09-23 21:47:57 +0200

    resolucion del ejercicio 4

---
+++ p1unidad2/ejercicio4.php
@@ -0,0 +1,25 @@
+<?php 
+/*
+*Identificar entre dos números aleatorios cual es el mayor y si este es par o impar.
+*Buscar información previamente para generar números aleatorios y usarla para
+*resolver el ejercicio.
+*/
+$numero1= rand(0,100);
+$numero2= rand(0,100);
+if($numero1>$numero2){
+    if($numero1%2==0){
+        echo $numero1." es mayor que ". $numero2."y ademas es par";
+    }
+    else{
+        echo $numero1." es mayor que ". $numero2."y ademas es impar";
+    }
+}else{
+    if($numero2%2==0){
+        echo $numero2." es mayor que ". $numero1."y ademas es par";
+    }
+    else{
+        echo $numero2." es mayor que ". $numero1."y ademas es impar";
+    }
+}
+
+?>
