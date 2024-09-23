fd905c306164e216ecbcee54b1112075ce755697
dariioosm <dariosanchez351@gmail.com>
2024-09-23 18:47:48 +0200

    resolucion del ejercicio 2

---
+++ p1unidad2/ejercicio2.php
@@ -0,0 +1,20 @@
+<?php 
+/*
+ *Dados 3 números asignados dentro del código a mostrar el número mayor de los tres
+ * 
+ * 
+*/
+$numero1=2;
+$numero2=3;
+$numero3=4;
+if($numero1<$numero2&&$numero1<$numero3){
+    echo 'el numero mayor es: '+$numero1;
+}
+if($numero2<$numero1&&$numero2<$numero3){
+    echo 'el numero mayor es: '+$numero2;
+}
+
+if($numero3<$numero1&&$numero3<$numero2){
+    echo 'el numero mayor es: '+$numero3;
+}
+?>
