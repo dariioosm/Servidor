1e3f3f833afe13e19d242e4dd25ad4dbbfe7dc20
Dario Sanchez <dariosanchez351@gmail.com>
2024-09-25 14:26:40 +0200

    comienzo del ejercicio8

---
+++ p1unidad2/ejercicio8.php
@@ -0,0 +1,11 @@
+<?php
+/*
+*Calcular si un número entero generado de forma aleatoria entre 1 y 1000 es
+*perfecto. Un número perfecto es aquel que la suma de sus divisores es él mismo,
+*por ejemplo el 6, sus divisores son 1,2,3 la suma de los mismos es 6
+ */
+$perfecto=6;
+for ($i=$perfecto;$i=0;$i--){
+    
+}
+?>
