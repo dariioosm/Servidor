d8d134cb2541bb0a8c26786da97147ee8ab698c4
Dario Sanchez <dariosanchez351@gmail.com>
2024-09-26 10:43:07 +0200

    primer formulario funcional jiij

--- tema1/array/datospersonales.php
+++ tema1/array/datospersonales.php
@@ -3,6 +3,6 @@
 /*
 * Crea un formulario con nombre, apellido y edad de un cliente y recuperalos en el servidor
  */
-echo "$_POST[$nombre][$apellido][$edad]";
+echo "$_POST Nombre: [$nombre] <br> Apellido: [$apellido] <br> Edad: [$edad]";
 
 ?>
