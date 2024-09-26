a346226370f37718ddca81b9a5f530dda4ed7dd2
Dario Sanchez <dariosanchez351@gmail.com>
2024-09-26 10:53:47 +0200

    primer formulario

--- tema1/array/datospersonales.php
+++ tema1/array/datospersonales.php
@@ -3,6 +3,8 @@
 /*
 * Crea un formulario con nombre, apellido y edad de un cliente y recuperalos en el servidor
  */
-echo "$_POST Nombre: [$nombre] <br> Apellido: [$apellido] <br> Edad: [$edad]";
+echo "Nombre: " . $_POST['nombre']."<br>";
+echo "Apellido:". $_POST['apellido'] ."<br>";
+echo "Edad:". $_POST['edad'];
 
 ?>
