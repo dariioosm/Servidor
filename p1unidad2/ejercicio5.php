aa4077d91f14f2588708da532a98a7e8f3480f39
Dario Sanchez <dariosanchez351@gmail.com>
2024-09-26 10:28:33 +0200

    comienzo de ejercicio 5

--- p1unidad2/ejercicio5.php
+++ p1unidad2/ejercicio5.php
@@ -11,6 +11,19 @@
 $a=0;
 $b=0;
 $c=0;
+$solucion1=0;
+$solucion2=0;
+if(((2*$b)-(4*$a*$c))<0){
+echo"No hay soluciones"
+}else if(((2*$b)-(4*$a*$c))>=0){
+    $solucion1=(-$b+sqrt(2*$b)-(4*$a*$c))/(2*$a);
+    $solucion2=(-$b+sqrt(2*$b)+(4*$a*$c))/(2*$a);
+    echo $solucion1;
+    echo $solucion2;
+}else if(((2*$b)-(4*$a*$c))!==0){
+    $solucion1=-$b/2$a;
+    echo $solucion1;
+}
 
 
 ?>
