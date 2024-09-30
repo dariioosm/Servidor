1307811966456a5eb3d6e4ec8f5695bd885b854f
Dario Sanchez <dariosanchez351@gmail.com>
2024-09-30 12:52:15 +0200

    comienzo del ejercicio9

---
+++ p1unidad2/ejercicio9.php
@@ -0,0 +1,10 @@
+<?php 
+/*
+*Escriba un programa a partir de un número entero generado de forma aleatoria
+*indique si es primo. Un número primo es aquel que es divisible por el mismo y la unidad. 
+*/
+$aleatorio= rand():int;
+
+
+
+?>
