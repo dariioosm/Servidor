aae5ee13916f980dc3807773a8ac410cca627958
dariioosm <dariosanchez351@gmail.com>
2024-09-30 17:50:09 +0200

    final del ejercicio 8

--- p1unidad2/ejercicio8.php
+++ p1unidad2/ejercicio8.php
@@ -4,8 +4,16 @@
 *perfecto. Un número perfecto es aquel que la suma de sus divisores es él mismo,
 *por ejemplo el 6, sus divisores son 1,2,3 la suma de los mismos es 6
  */
-$perfecto=6;
-for ($i=$perfecto;$i=0;$i--){
-    
+$perfecto=rand(1,1000);
+$suma=0;
+for ($i=1;$i<$perfecto;$i++){
+    if($perfecto%$i==0){
+        $suma+=$i;
+    }
+}
+if($suma==$perfecto){
+    echo "$perfecto es un numero perfecto";
+}else{
+    echo "$perfecto no es un numero perfecto";
 }
 ?>
