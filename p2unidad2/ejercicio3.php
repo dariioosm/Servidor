630361e604200d1885b37b7455c1119cc03c4264
dariioosm <dariosanchez351@gmail.com>
2024-10-01 22:18:23 +0200

    comienzo ejercicio 3

---
+++ p2unidad2/ejercicio3.php
@@ -0,0 +1,11 @@
+<?php 
+/*
+* Almacena en un array los 10 primeros numeros pares e imprimelos cada uno en una fila
+*/
+for($i=0;$i<=20;$i++){
+    if($i%2==0){
+        $vector= array($i);
+    }
+}
+var_dump($vector);
+?>
