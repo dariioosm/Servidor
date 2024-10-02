e19dea012946c28636a5382501f823baf56f43fa
dariioosm <dariosanchez351@gmail.com>
2024-10-02 22:03:30 +0200

    creaccion del ejercicio 9 y primer planteamiento del 10

---
+++ p2unidad2/ejercicio10.php
@@ -0,0 +1,22 @@
+<?php 
+/*
+*Carga el siguiente vector e imprime los valores del array asociativo usando la
+*estructura de control foreach:
+*$v[1]=90;
+*$v[30]=7;
+*$v[‘e’]=99;
+*$v[‘hola’]=43;
+*/
+$v[1]=90;
+$v[30]=7;
+$v = array(
+    'e'=>99,
+    'hola'=>43
+);
+foreach ($v as $key => $value) {
+    echo $value;
+    //?con esto sólo se muestran 99 y 43 no sé cómo sacar 90 y 7 :(
+}
+
+
+?>
---
+++ p2unidad2/ejercicio9.php
@@ -0,0 +1,23 @@
+<?php 
+/*
+*Llenar una matriz de 20x20 con valores aleatorios. Sumar las columnas e
+*imprimir la columna que tuvo la máxima suma y la suma de esa columna
+*/
+$matriz=array();
+for($i=0;$i<19;$i++){
+    for($j=0;$j<19;$j++){
+        $matriz[$i][$j]=rand(1,400);
+        echo $matriz[$i][$j]."\t";
+    }
+    echo"<br>";
+}
+
+//TODO suma por columnas
+
+$sumaColumnas=array();
+for(){
+    
+}
+
+
+?>
