5b50b45e56312cbaaf7a1835506eae5c4c4edb3d
Dario Sanchez <dariosanchez351@gmail.com>
2024-10-02 13:59:40 +0200

    usos de foreach

---
+++ pruebas/foreach.php
@@ -0,0 +1,21 @@
+<?php 
+/*
+*$mascotas= array('perros'=>"yunito",
+*                 'gato'=> "wilson",
+*                 'canario'=> "piolin",
+*                 'tortuga'=>"mbape"); 
+*foreach($mascotas as $animal => $description)
+*echo "$animal : $description"."<br>"
+*/
+$mascotas=array(
+    'perros'=> array('mastin'=>'yunito','salchicha'=>'fuet','chiguagua'=>'sarnoso'),
+    'gatos'=> array('persa'=>'otis','comun'=>'garfield','siames'=>'princesa')
+);
+foreach($mascotas as $animal => $description){
+    echo "$animal <br> ********* <br>";
+    foreach($description as $tipo => $raza){
+        echo "$tipo:$raza"."<br>"."<br>";
+    }
+}
+
+?>
