cce9daa3990ed21ac3c3a9bf103706e838309667
Dario Sanchez <dariosanchez351@gmail.com>
2024-10-03 12:33:03 +0200

    reasignacion de nombres

--- p2unidad2/ejercicio17.php
+++ p2unidad2/ejercicio17.php
@@ -1,12 +1,12 @@
 <?php 
 /*
-*Realiza el ejercicio anterior pero con la funicón array_push().
- */
-
+*Rellena los siguientes tres arrays y júntalos en uno nuevo. Muéstralos porpantalla. 
+*Utiliza la función array_merge()
+*/
 $animales=array("Lagatija","Araña","Perro","Gato","Raton");
 $numeros=array("12","34","45","52","12");
 $arboles=array("Sauce","Pino","Naranjo","Chopo","Perro","34");
-$total=array();
-array_push($total,$animales,$numeros,$arboles);
+$total= array_merge($animales,$numeros,$arboles);
 print_r($total);
+
 ?>
--- p2unidad2/ejercicio18.php
+++ p2unidad2/ejercicio18.php
@@ -1,12 +1,12 @@
 <?php 
 /*
-*Rellena los siguientes tres arrays y júntalos en uno nuevo. Muéstralos porpantalla. 
-*Utiliza la función array_merge()
-*/
+*Realiza el ejercicio anterior pero con la funicón array_push().
+ */
+
 $animales=array("Lagatija","Araña","Perro","Gato","Raton");
 $numeros=array("12","34","45","52","12");
 $arboles=array("Sauce","Pino","Naranjo","Chopo","Perro","34");
-$total= array_merge($animales,$numeros,$arboles);
+$total=array();
+array_push($total,$animales,$numeros,$arboles);
 print_r($total);
-
 ?>
