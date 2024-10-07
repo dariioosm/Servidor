b03342d912dccfbc6af8dafc21608cdfb3aa7b19
Dario Sanchez <dariosanchez351@gmail.com>
2024-10-07 14:23:48 +0200

    comienzo del ejercicio 4

---
+++ p3unidad2/ejercicio04.php
@@ -0,0 +1,19 @@
+<?php
+/*
+*Escribe una función que reciba un array de números, y un número, el límite. 
+*La función tiene que devolver un nuevo array que contenga solo los elementos del array original menores que el límite.
+*/
+
+$M= array(
+    array(1,0,0,2),
+    array(0,1,1,0),
+    array(0,1,1,0),
+    array(2,0,0,1),
+);
+$maximo=2;
+
+function limite($M,$maximo){
+    
+}
+
+?>
