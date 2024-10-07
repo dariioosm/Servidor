ee38f654ac8c12f0b4b17d35b388c2ca2d27f108
dariioosm <dariosanchez351@gmail.com>
2024-10-07 20:21:16 +0200

    comienzo de ejercicio 5

---
+++ p3unidad2/ejercicio05.php
@@ -0,0 +1,39 @@
+<?php 
+/*
+*Escribe un script para probar algunas de las funciones mostradas debajo, el script ha de contener al menos tres funciones de cada bloque
+*   Funciones de variables
+*       isset($var) TRUE si la variable está inicializada y no es NULL
+*       is_null($var) TRUE si la variable es NULL
+*       empty($var) TRUE si la variable no está inicializada o su valor es FALSE Para comprobar el tipo de dato de $var
+*       is_int($var), is_float($var), Para comprobar el tipo de dato de $var
+*       is_bool($var), is_array($var) Para comprobar el tipo de dato de $var
+*       intval($var), floatval($var), boolvar($var), strval($var) Para obtener el valor de $var como otro tipo de dato
+
+*   Funciones de cadenas
+*        strlen($cad) Devuelve la longitud de $cad
+*        explode($cad, $token) Parte una cadena utilizando $token como separador. Devuelve un array de cadenas
+*        implode($token, $array) Crea una cadena larga a partir de un array de cadenas, entre cadena y cadena se introduce $token
+*        strcmp($cad1, $cad2) Compara las dos cadenas. Devuelve 0 si son iguales, -1 si $cad1 es menor y 1si $cad1 es mayor
+*        strtolower($cad), Devuelven $cad en mayúsculas o minúsculas, respectivamente
+*        strtoupper($cad)  Devuelven $cad en mayúsculas o minúsculas, respectivamente
+*        str($cad1, $cad2) Busca la primera ocurrencia de $cad2 en $cad1. Si no aparece devuelve FALSE, si aparece devuelve $cad1 desde donde comienza la ocurrencia
+
+*   Funciones de array
+*       ksort($arr), krsort($arr) Ordena el array por clave en orden ascendente o descendente
+*       sort($arr), rsort($arr) Ordena el array por valor en orden ascendente o descendente Devuelve los valores de $arr
+*       array_values($arr) Devuelve los valores de $arr
+*       array_keys($arr) Devuelve las claves de $arr
+*       array_key_exists($arr, $cla) Devuelve verdadero si algún elemento de $arr tiene clave $cla Devuelve el número de elementos del array
+*       count($arr) Devuelve numero de elementos de array
+*/
+
+//TODO funciones de variables
+
+
+
+
+
+
+
+
+?>
