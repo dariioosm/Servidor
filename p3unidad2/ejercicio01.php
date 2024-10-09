284134293b836615a4b12f63e6386a7cae77c802
Dario Sanchez <dariosanchez351@gmail.com>
2024-10-09 14:25:27 +0200

    cosa

--- p3unidad2/ejercicio01.php
+++ p3unidad2/ejercicio01.php
@@ -3,31 +3,36 @@
 *Crea una función para resolver la ecuación de segundo grado. Esta función recibe los coeficientes de la ecuación y devuelve un array con las soluciones. 
 *Si no hay soluciones reales, devuelve FALSE.
 */
-    $a=0;
-    $b=0;
-    $c=0;
-    $is_real=false;
-function resultado($a, $b,$c){
+    $a=6;
+    $b=7;
+    $c=8;
+    $is_real=true;
     $solucion1=0;
     $solucion2=0;
     
-    if(((2*$b)-(4*$a*$c))<0){
-    echo"No hay soluciones";
-    return false;
-    }else if(((2*$b)-(4*$a*$c))>=0){
+function resultado($a, $b,$c){
+    
+    if(((2**$b)-(4*$a*$c))<0){
+        $is_real=false;
+    }else if(((2**$b)-(4*$a*$c))>=0){
         $solucion1=(-$b+sqrt(2*$b)-(4*$a*$c))/(2*$a);
         $solucion2=(-$b+sqrt(2*$b)+(4*$a*$c))/(2*$a);
-        return array($solucion1,$solucion2);
-    }else if(((2*$b)-(4*$a*$c))!==0){
-        $solucion1=-$b/2*$a;
-        
-        return $solucion1;
+        //$is_real=true;
+    }else if(((2**$b)-(4*$a*$c))!==0){
+        $solucion1=-$b/(2*$a);
+        //$is_real=true;
     }
+    return array($solucion1,$solucion2);
 }
+$resultados=resultado($a,$b,$c);
 
-if($is_real==true){
-    echo $solucion1, $solucion2;
-}else{
+if($is_real=false){
     echo "no hay soluciones posibles";
+}else{
+    if(count($resultados)==1){
+        echo $resultados[0];
+    }else{
+        echo $resultados[0].' y '.$resultados[1];
+    }
 }
 ?>
