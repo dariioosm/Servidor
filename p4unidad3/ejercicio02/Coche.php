5454046579860c98b9ea25f093f485d45641045d
Dario Sanchez <dariosanchez351@gmail.com>
2024-10-14 12:35:14 +0200

    solucion de ejercicio 2

---
+++ p4unidad3/ejercicio02/Coche.php
@@ -0,0 +1,13 @@
+<?php 
+class Coche{
+    public $numero_cadenas_nieve;
+    public function annadir_cadenas_nieve($num){
+
+    }
+    public function quitar_cadenas_nieve($num){
+
+    }
+}
+
+
+?>
