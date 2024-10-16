b2351bb7b20f53b02cd16b8cbfee8981d586d3ab
Dario Sanchez <dariosanchez351@gmail.com>
2024-10-16 14:01:06 +0200

    arreglo en la entrada de parametros del constructor

--- p4unidad3/ejercicio03/Mostrar.php
+++ p4unidad3/ejercicio03/Mostrar.php
@@ -2,6 +2,8 @@
 require('Vehiculo.php');
 $miVehiculo= new Vehiculo(1500,'negro');
 echo '<br>';
+echo $miVehiculo;
+echo '<br>';
 echo $miVehiculo -> annadir_persona(70);
 echo '<br>';
 echo $miVehiculo -> circula();
--- p4unidad3/ejercicio03/Vehiculo.php
+++ p4unidad3/ejercicio03/Vehiculo.php
@@ -1,39 +1,40 @@
 <?php
 class Vehiculo{
-    protected $color;
-    protected $peso;
+    public $color;
+    public $peso;
 
     //TODO poner getter y setter de los atributos
-    protected function setColor($color){
+    public function setColor($color){
         $this->color = $color;
     }
 
-    protected function getColor(){
+    public function getColor(){
         return $this -> color;
     }
-    protected function setPeso($peso){
+    public function setPeso($peso){
         $this->peso = $peso;
     }
 
     
-    protected function getPeso(){
+    public function getPeso(){
         return $this -> peso;
     }
 
     //TODO  constructor en la clase vehiculo  que pase color y peso
 
-    protected function __construct($color,$peso)
+    public function __construct($peso,$color)
     {
         $this->color=$color;
         $this->peso=$peso;
     }
 
-    protected function circula(){
+    public function circula(){
         echo 'el vehiculo circula <br>';
     }
 
-    protected function annadir_persona($peso_persona){
-        $this ->peso = $this->peso + $peso_persona;
+    public function annadir_persona($peso_persona){
+        $this -> peso = $this->peso + $peso_persona;
+        return 'Se annadio una persona';
     }
     
     
@@ -41,7 +42,7 @@ class Vehiculo{
 
     public function __toString()
     {
-        return 'Peso y color: '.$this->peso.''.$this->color;
+        return 'Peso y color: '.$this->peso.' '.$this->color;
     }
 }
 
