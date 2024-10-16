5a8c5526edccbf3ddf55c8dfce85b07af8048397
Dario Sanchez <dariosanchez351@gmail.com>
2024-10-16 13:36:40 +0200

    logica ejercicio 3

--- p4unidad3/ejercicio03/Mostrar.php
+++ p4unidad3/ejercicio03/Mostrar.php
@@ -1,7 +1,10 @@
 <?php 
 require('Vehiculo.php');
 $miVehiculo= new Vehiculo(1500,'negro');
+echo '<br>';
 echo $miVehiculo -> annadir_persona(70);
+echo '<br>';
 echo $miVehiculo -> circula();
+echo '<br>';
 echo $miVehiculo;
 ?>
