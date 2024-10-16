67f5248e9d29a05a4503199f5cd365da7ca42c74
Dario Sanchez <dariosanchez351@gmail.com>
2024-10-16 12:55:03 +0200

    modificacion del ejercicio 3

--- p4unidad3/ejercicio03/Dos_ruedas.php
+++
@@ -1,9 +0,0 @@
-<?php 
-class Dos_ruedas extends Vehiculo{
-    protected $cilindrada;
-    protected function poner_gasolina($litros){
-        
-    }
-}
-
-?>
--- p4unidad3/ejercicio03/mostrar.php
+++
@@ -1,7 +0,0 @@
-<?php 
-include 'Vehiculo.php';
-$vehiculo = new Vehiculo('negro', 1500);
-$vehiculo ->circula(true);
-$vehiculo->annadir_persona(70);
-
-?>
