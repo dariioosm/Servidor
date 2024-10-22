5eebe5a3f3c1d59b0ae992d7a1c2927258178d08
dariioosm <dariosanchez351@gmail.com>
2024-10-22 23:41:47 +0200

    fin ejercicio 02

--- Formularios/ejercicio02/consulta.php
+++ Formularios/ejercicio02/consulta.php
@@ -1,22 +1,26 @@
 <?php 
-switch ($_POST['name']){
-    case '14':
-        echo 'eres una personita';
-        break;
-    case '15':
-        echo 'todavía eres muy joven';
-        break;
-    case '21':
-        echo 'eres una persona joven';
-        break;
-    case '41':
-        echo 'eres una persona madura';
-        break;
-    case '61':
-        echo 'Ya eres una persona mayor';
-        break;
-    default:
-        echo 'Aún no me has dicho tu edad';
-    }
+if(isset($_POST["edad"])){
+    $edad = intval($_POST["edad"]);
+    switch ($_POST["edad"]){
+        case '14':
+            echo 'eres una personita';
+            break;
+        case '15-20':
+            echo 'todavía eres muy joven';
+            break;
+        case '21-40':
+            echo 'eres una persona joven';
+            break;
+        case '41-60':
+            echo 'eres una persona madura';
+            break;
+        case '61+':
+            echo 'Ya eres una persona mayor';
+            break;
+        }
+    
+}else{
+    echo 'Aún no me has dicho tu edad';
+}
 
 ?>
