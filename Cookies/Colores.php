1e4c189798f24858c2bae4cdf87ab3476c774043
Dario Sanchez <dariosanchez351@gmail.com>
2024-11-18 14:22:26 +0100

    comienzo de coookies

---
+++ Cookies/Colores.php
@@ -0,0 +1,13 @@
+<?php
+
+if(!isset($_COOKIE['color'])){
+    $color='white'; 
+}else{
+    $color=$_COOKIE['color'];
+}
+?>
+<form action="" method="post">
+    <label for="color" type="radio"></label>
+    <label for="color" type="radio"></label>
+    <label for="color" type="radio"></label>
+</form>
