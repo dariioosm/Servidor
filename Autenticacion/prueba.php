e5d961c8c4377e32efd90df8c24c48d2b9cc810f
Dario Sanchez <dariosanchez351@gmail.com>
2024-11-20 13:55:04 +0100

    1st authentication

---
+++ Autenticacion/prueba.php
@@ -0,0 +1,11 @@
+<?php 
+$user='pepito';
+$passwd='132';
+
+//TODO validacion de la entrada de datos con el registro guardado
+if(isset($_POST['user']) && isset($_POST['pass'])){
+    echo'User & Pass exist';
+}else{
+    echo'combinacion fallida';
+}
+?>
