42ca69ab19e6751e98a9a245929d7d22c4533660
dariioosm <dariosanchez351@gmail.com>
2024-11-20 18:57:51 +0100

    desarrollo parcial de la logica del registro de usuario

--- Autenticacion/prueba2.php
+++ Autenticacion/prueba2.php
@@ -1,5 +1,24 @@
 <?php
 session_start();
+//TODO guardar los post del formulario del registro en las sesiones
+
+$_SESSION['alta']=$_POST['user'];
+$_SESSION['passwd']=$_POST['passwd'];
+$_SESSION['passwd2']=$_POST['passwd2'];
+$_SESSION['rol']=$_POST['rol'];
+
+
+//TODO comprobar que las contraseñas enviadas sean iguales
+if($_SESSION['passwd']===$_SESSION['passwd2']){
+    $user=$_SESSION['alta'];
+    $passwd=$_SESSION['passwd2'];
+    $rol=$_SESSION['rol']; 
+    echo'La cuenta ha sido creada correctamente';
+}else{
+    echo 'Las contraseñas escritas no coinciden';
+}
+
+
 
 
 ?>
--- Autenticacion/registro.php
+++ Autenticacion/registro.php
@@ -8,20 +8,20 @@
 <body>
     <form action="prueba2.php" method="post">
         <label for="">Usuario</label>
-        <input type="text" name='user' id="user" require>
+        <input type="text" name='user' id="user" required>
         <br><br>
         <label for="">Contraseña</label>
-        <input type="password" name="passwd" id="passwd" require>
+        <input type="password" name="passwd" id="passwd" required>
         <br><br>
         <label for="">Confirmacion de contraseña</label>
-        <input type="password" name="passwd2" id="passwd2" require>
+        <input type="password" name="passwd2" id="passwd2" required>
         <br><br>
         <label for="">Rol</label>
         <br><br>
         <label for="">Estandar</label>
-        <input type="radio" name="std" id="std">
+        <input type="radio" name="rol" id="std" value="std">
         <label for="">Prime</label>
-        <input type="radio" name="prime" id="prime">
+        <input type="radio" name="rol" id="prime" value="prime">
         <br> <br>
         <input type="submit" value="Registrarse">
     </form>
