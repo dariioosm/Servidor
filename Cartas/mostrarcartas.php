7c070750ac5960fb74c3d82d731ea5ae688b533c
Dario Sanchez <dariosanchez351@gmail.com>
2024-12-11 13:54:55 +0100

    -

--- Cartas/mostrarcartas.php
+++ Cartas/mostrarcartas.php
@@ -1,25 +1,5 @@
 <?php
 session_start();
-//TODO mostrar las cartas boca abajo
-
-
-/*for($i=0;$i<5;$i++){
-    echo"<img height=20px src='img/boca_abajo.jpg'/>";
-}*/
-
-//TODO sacar las imagenes de las cartas usando matriz con vectores
-$todasCombinaciones=[
-    1=>['img/copas_03.jpg','img/copas_02.jpg','img/copas_02.jpg','img/copas_03.jpg','img/copas_05.jpg','img/copas_05.jpg'],
-    2=>['img/copas_02.jpg','img/copas_02.jpg','img/copas_05.jpg','img/copas_03.jpg','img/copas_05.jpg','img/copas_03.jpg'],
-    3=>['img/copas_02.jpg', 'img/copas_03.jpg','img/copas_05.jpg','img/copas_05.jpg','img/copas_03.jpg','img/copas_02.jpg']
-]; 
-$aleatorio=rand(1,3);
-
-foreach($todasCombinaciones [$combinacion]as$carta){
-    if($aleatorio==$combinacion){
-        echo"<img src='$carta' height='100px'/>";
-    }
-}
 ?>
 
 <!DOCTYPE html>
@@ -37,3 +17,28 @@ foreach($todasCombinaciones [$combinacion]as$carta){
     </form>
 </body>
 </html>
+
+<?php
+session_start();
+if (isset($_SESSION['login'])) {
+    echo "<h1>Bienvenido/a " . $_SESSION['login'] . "</h1>";
+}
+//TODO mostrar las cartas boca abajo
+
+
+for($i=0;$i<5;$i++){
+    echo"<img height=20px src='img/boca_abajo.jpg'/>";
+}
+
+//TODO sacar las imagenes de las cartas usando matriz con vectores
+$todasCombinaciones=[
+    1=>['img/copas_03.jpg','img/copas_02.jpg','img/copas_02.jpg','img/copas_03.jpg','img/copas_05.jpg','img/copas_05.jpg'],
+    2=>['img/copas_02.jpg','img/copas_02.jpg','img/copas_05.jpg','img/copas_03.jpg','img/copas_05.jpg','img/copas_03.jpg'],
+    3=>['img/copas_02.jpg', 'img/copas_03.jpg','img/copas_05.jpg','img/copas_05.jpg','img/copas_03.jpg','img/copas_02.jpg']
+]; 
+$aleatorio=rand(1,3);
+$_SESSION['correcto']=$todasCombinaciones [$aleatorio];
+foreach( $_SESSION['correcto'] as $carta){
+        echo"<img src='$carta' height='100px'/>";
+    }
+?>  
