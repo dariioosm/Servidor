bf5fc8de824305bf7db367dabe86cdb9903289c3
Dario Sanchez <dariosanchez351@gmail.com>
2024-12-19 13:31:26 +0100

    -

--- ArbolNavidadFicheros/subida.php
+++ ArbolNavidadFicheros/subida.php
@@ -1,70 +1,35 @@
 <?php 
 $directory="uploads/";
-$target_file=$directory.basename($_FILES["archivoSubir"]["name"]);
-$tipoArchivo=pathinfo($target_file,PATHINFO_EXTENSION);
 $subidaOk=1;
-//TODO comprobar tipo de archivo
-if($tipoArchivo !="jpg" && $tipoArchivo !="png" && $tipoArchivo !="jpeg" && $tipoArchivo !="gif" && $tipoArchivo !="jfif"){
-    echo "solo se puede subir archivos de imagen";
-    $subidaOk=0;
-}
-//TODO comprobar si hay un error en la subida de archivo
-if($_FILES["archivoSubir"]["error"]!=0){
-    echo "Error al subir el archivo: ". $_FILES["archivoSubir"]["error"];
-    $subidaOk=0;
-}
+//TODO comprobar si el formulario se ha enviado
+if(isset($_FILES['archivoSubir'])){
+    $archivosSubidos=[];
+    //TODO hacer un bucle para recorrer las imagenes
+    foreach($_FILES['archivoSubir']["name"] as $index=>$contenido){
+        $archivoDestino= $directory.basename($_FILES["archivoSubir"]["name"][$contenido]);
+        $tipoArchivo=pathinfo($archivoDestino,PATHINFO_EXTENSION);
 
-
-//TODO comprobar si es real o no el archivo subido
-$comprueba = getimagesize($_FILES["archivoSubir"]["tmp_name"]);
-if($comprueba!== false){
-    echo "El fichero es una imagen".$comprueba["mime"];
-    $subidaOk=1;
-}else{
-    echo "El fichero seleccionado no es una imagen";
-    $subidaOk=0;
-}
-//TODO mover el archivo al destino si las comprobaciones ✔
-if($subidaOk==1){
-    if(move_uploaded_file($_FILES["archivoSubir"]["tmp_name"],$target_file)){
-        echo "<table>";
-        //TODO recorrer los 4 niveles del arbol
-        for($i=0;$i<4;$i++){
-            echo "<tr>;";
-                //TODO moverme por los tr y poner los td con la imagen guardada en uploads
-                for($j=0;$j<4;$j++){
-                    echo"<td>";
-                    echo "<img src='".$target_file. "'>";
-                    echo"</td>";
-                }
-            echo"</tr>";
+        //TODO comprobar que las extensiones en el contenido sean formatos de imagen
+        //? funcion in_array busca la "aguja en el pajar" => busca los parametros que le he indicado(en este caso formatos de imagen)
+        if(!in_array($tipoArchivo,['jpg', 'png', 'jpeg', 'gif', 'jfif'])){
+            $subidaOk=0;
+            echo"Solo se pueden subir archivos de imagen";
         }
-        echo "</table>";
+        //TODO comprobar errores en la subida de archivos a servidor
+        if($_FILES["archivoSubir"]["error"]!=0){
+            echo "Error al subir el archivo: " . $_FILES["archivoSubir"]["error"];
+            $subidaOk = 0;
+        }
+        //TODO mover los archivos a la carpeta uploads
+        if($subidaOk==1){
+            if(move_uploaded_file($_FILES["archivoSubir"]["tmp_name"], $archivoDestino){
+
+            }
+        }
+
     }
 }
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <table>
-        <tr>
-            <td colspan="1"><img src="uploads/" alt=""></td>
-        </tr>
-        <tr>
-            <td colspan="2"><img src="uploads/" alt=""></td>
-        </tr>
-        <tr>
-            <td colspan="3"><img src="uploads/" alt=""></td>
-        </tr>
-        <tr>
-            <td colspan="4"><img src="uploads/" alt=""></td>
-        </tr>
-    </table>
-</body>
-</html>
+
+
+?>
