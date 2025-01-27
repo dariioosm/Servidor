f02bd8fe19e60a650a58cc1786d715762efedd6f
Dario Sanchez <dariosanchez351@gmail.com>
2025-01-27 14:26:37 +0100

    .

--- MONGODB/AytoGijon/PerrosSQL.php
+++ MONGODB/AytoGijon/PerrosSQL.php
@@ -1,9 +1,18 @@
 <?php
+//conexion a mysql
 $servidor = "localhost:8080";
 $usuario = "root";
 $contraseña = "";
 $bdsql = 'perros';
 
+//conexion a mongodb
+
+require '../vendor/autoload.php';
+$clienteMongo = new MongoDB\Client('mongodb://localhost:27017');
+$bdMongo= $clienteMongo->$perros;
+$coleccion_perros=$bdMongo->peligrosos;
+
+$sql
 
 
 
