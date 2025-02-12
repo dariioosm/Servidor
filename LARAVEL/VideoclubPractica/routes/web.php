6e551110615c562e0ca37fdeb0faf79c4c91a242
Dario Sanchez <dariosanchez351@gmail.com>
2025-02-12 14:09:19 +0100

    creacion de vistas blade

--- LARAVEL/VideoclubPractica/routes/web.php
+++ LARAVEL/VideoclubPractica/routes/web.php
@@ -3,29 +3,24 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    echo 'Pantalla principal';
     return view('home');
 });
 
 Route::get('login',function(){
-    echo 'login usuario';
     return view('auth.login');
 });
 Route::get('logout',function(){
-    echo 'logout usuario';
+    return view('auth.login');
 });
 Route::get('catalog',function(){
-    echo 'Listado de peliculas';
     return view('catalog.index');
 });
-Route::get('catalog/show{id}',function($id){
-    echo 'Vista detalle '.$id;
+Route::get('catalog/show/{id}',function($id){
     return view('catalog.show',array('id'=>$id));
 });
 Route::get('catalog/create',function(){
     return view('catalog.create');
 });
 Route::get('catalog/edit/{id}',function($id){
-    echo 'Modificar pelicula '.$id;
     return view('catalog.edit');
 });
