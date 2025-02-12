a78081d37570fb407db298d538cd6ba2b044f1c2
Dario Sanchez <dariosanchez351@gmail.com>
2025-02-12 13:53:21 +0100

    creacion de carpetas

--- LARAVEL/VideoclubPractica/routes/web.php
+++ LARAVEL/VideoclubPractica/routes/web.php
@@ -4,26 +4,28 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     echo 'Pantalla principal';
-    return('home');
+    return view('home');
 });
 
 Route::get('login',function(){
     echo 'login usuario';
+    return view('auth.login');
 });
 Route::get('logout',function(){
     echo 'logout usuario';
 });
 Route::get('catalog',function(){
     echo 'Listado de peliculas';
-    return('catalog.inndex');
+    return view('catalog.index');
 });
 Route::get('catalog/show{id}',function($id){
     echo 'Vista detalle '.$id;
     return view('catalog.show',array('id'=>$id));
 });
 Route::get('catalog/create',function(){
-    echo 'Añadir pelicula';
+    return view('catalog.create');
 });
 Route::get('catalog/edit/{id}',function($id){
     echo 'Modificar pelicula '.$id;
+    return view('catalog.edit');
 });
