69a488c6064b5423e8d1ff6dd39770616cd96f1b
Dario Sanchez <dariosanchez351@gmail.com>
2025-02-12 12:57:37 +0100

    .

--- DiabetesApp/php/home.php
+++ DiabetesApp/php/home.php
@@ -11,10 +11,10 @@
     <h2>Welcome <?php session_start(); echo $_SESSION['usuario'] ?></h2>
     <div class="container-fluid">
 <!--Formulario de control_glucosa-->
-//! en el formulario de inserccion tiene que coger por defecto la fecha del sistema
-//TODO poner un valor fijo en la insulina lenta y ponerle un boton que se pueda modificar
-//TODO control de glucosa con fecha de sistema que no se modifica 
-//TODO hiper e hipoglucosa comparten formulario y si se elige hiper, se muestra la correccion de insulina, si no, no se muestra la correccion de insulina
+<!--//! en el formulario de inserccion tiene que coger por defecto la fecha del sistema-->
+<!--//TODO poner un valor fijo en la insulina lenta y ponerle un boton que se pueda modificar-->
+<!--//TODO control de glucosa con fecha de sistema que no se modifica -->
+<!--//TODO hiper e hipoglucosa comparten formulario y si se elige hiper, se muestra la correccion de insulina, si no, no se muestra la correccion de insulina-->
         <div class="row">
             <div class="col-4 mt-2 border border-primary-subtle rounded">
                 <form action="#" method="post">
--- DiabetesApp/php/login.php
+++ DiabetesApp/php/login.php
@@ -50,11 +50,14 @@ if ($conn->connect_error) {
         if(empty($user)||empty($pass)){
             echo 'Faltan datos por rellenar';
         }else{
+
             //TODO hacer comprobacion inversa (comprobar si no existe en bbdd)
             $existeuser =$conn -> prepare('SELECT * FROM usuarios where login=?');
             $existeuser->bind_param('s',$user);
             $existeuser ->execute();
             $resultado=$existeuser->get_result();
+            //! hacer comprobacion de la combinacion de login y passwd correcta            
+
             if($resultado ->num_rows <1){
                 echo 'Este nombre de usuario no existe <a href="../index.php">registrate  aqui </a>';
             }else{
