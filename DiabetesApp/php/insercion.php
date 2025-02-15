fde98ee980f76e16937b5f99fe9e458d9a6dc380
dariioosm <dariosanchez351@gmail.com>
2025-02-15 01:26:00 +0100

    Añadir script de inserción de datos para el control de glucosa y manejo de hiperglucemia/hipoglucemia

---
+++ DiabetesApp/php/insercion.php
@@ -0,0 +1,82 @@
+<?php
+require 'conexion.php';
+
+//TODO recoger los datos del formulario
+if($_SERVER['REQUEST_METHOD']=='POST'){
+
+    $fecha_control = $_POST['fecha_control'];
+    $tipo_comida = $_POST['tipo_comida'];
+    $raciones = $_POST['raciones'];
+    $glucosa_pre = $_POST['glucosa_preingesta'];
+    $insulina = $_POST['insulina'];
+    $glucosa_pos = $_POST['glucosa_postingesta'];
+    
+    //?Posibles datos de las tablas hiper e hipoglucemia (puede esta vacia la tabla)
+    
+    $glucosa_estado = $_POST['glucosa_estado'] ?? '';
+
+    $glucosa_hiper = $_POST['glucosa_hiper'] ?? null;
+    $hora_hiper = $_POST['hora_hiper'] ?? null;
+    $unidades_correccion = $_POST['unidades_correccion'] ?? null;
+
+    $glucosa_hipo = $_POST['glucosa_hipo'] ?? null;
+    $hora_hipo = $_POST['hora_hipo'] ?? null;
+
+    //TODO comprobacion de campos obligatorios
+    if(empty($tipo_comida)||empty($tipo_comida)||empty($raciones)||empty($glucosa_pre)||empty($insulina)||empty($glucosa_pos)){
+        echo 'Campos obligatorios sin rellenar';
+    }else{
+        //TODO insercion en tabla comida
+
+        $select_id= $conn ->prepare('SELECT id_usuario FROM USUARIOS WHERE login LIKE ?');
+        $select_id->bind_param('s',$_SESSION['usuario']);
+        $select_id->execute();
+        $select_id->bind_result($id_usuario);
+        $select_id->fetch();
+        $select_id->close();
+
+        if(!$id_usuario){
+            echo 'Usuario no encontrado';
+            exit;
+        }
+
+        $insert_comida= $conn -> prepare('INSERT INTO COMIDA (id_usuario,fecha_control,tipo_comida,raciones,glucosa_preingesta,insulina,glucosa_postingesta) VALUES(?,?,?,?,?,?,?)');
+        $insert_comida ->bind_param('issiiii',$id_usuario,$fecha_control,$tipo_comida,$raciones,$glucosa_pre,$insulina,$glucosa_pos);
+        
+        if($insert_comida->execute()){
+            echo 'Datos insertados correctamente en la tabla';
+        }else{
+            echo'Error al insertar datos en la tabla'. $insert_comida->error;
+        }
+
+        //TODO insertar en tabla hiperglucemia
+
+        if($glucosa_estado == 'hiperglucemia' && !empty($glucosa_hiper) && !empty($hora_hiper) && !empty($unidades_correccion)){
+            $insert_hiper = $conn -> prepare('INSERT INTO HIPERGLUCEMIA (id_usuario, fecha_control, tipo_comida, glucosa_hiper, hora_hiper, unidades_correccion) VALUES (?, ?, ?, ?, ?, ?))');
+            $insert_hiper->bind_param('issisi', $id_usuario, $fecha_control, $tipo_comida, $glucosa_hiper, $hora_hiper, $unidades_correccion);
+            if ($insert_hiper->execute()) {
+                echo 'Datos insertados correctamente en la tabla HIPERGLUCEMIA.<br>';
+            } else {
+                echo 'Error al insertar datos en HIPERGLUCEMIA: ' . $insert_hiper->error;
+            }
+            $insert_hiper->close();
+        }
+        //TODO inertar en tabla hipoglucemia
+        if ($glucosa_estado == 'hipoglucemia' && !empty($glucosa_hipo) && !empty($hora_hipo)) {
+            $insert_hipo = $conn->prepare('INSERT INTO HIPOGLUCEMIA (id_usuario, fecha_control, tipo_comida, glucosa_hipo, hora_hipo) VALUES (?, ?, ?, ?, ?)');
+            $insert_hipo->bind_param('issis', $id_usuario, $fecha_control, $tipo_comida, $glucosa_hipo, $hora_hipo);
+    
+            if ($insert_hipo->execute()) {
+                echo 'Datos insertados correctamente en la tabla HIPOGLUCEMIA.<br>';
+            } else {
+                echo 'Error al insertar datos en HIPOGLUCEMIA: ' . $insert_hipo->error;
+            }
+            $insert_hipo->close();
+        }
+    }
+    $insert_comida->close();
+    $conn -> close();
+}
+
+
+?>
