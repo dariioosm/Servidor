d4a01a3c1f0006215b444419099ae2f5d1c4b72b
Dario Sanchez <dariosanchez351@gmail.com>
2025-02-26 13:43:49 +0100

    Add HomeController and MoviesController to handle home and movies functionality

--- LARAVEL/VideoclubPractica/routes/web.php
+++ LARAVEL/VideoclubPractica/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\CatalogController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('catalog',[CatalogController::class,'getIndex']);
+Route::get('catalog/show/{id}',[CatalogController::class,'getShow']);
+Route::get('catalog/create',[CatalogController::class,'getCreate']);
+Route::get('catalog/edit/{id}',[CatalogController::class,'getEdit']);
