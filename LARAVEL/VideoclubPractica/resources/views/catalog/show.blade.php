6887c9d53417eca431c98adfb63c2e80737f753d
Dario Sanchez <dariosanchez351@gmail.com>
2025-02-26 14:21:53 +0100

    Refactor show.blade.php view file

---
+++ LARAVEL/VideoclubPractica/resources/views/catalog/show.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE>
+<html>
+<body>
+<div class="row">
+<div class="col-sm-4">
+{{-- TODO: Imagen de la película --}}
+
+<h1><?php $arrayPeliculas['titulo']  ?></h1>
+</div>
+<div class="col-sm-8">
+{{-- TODO: Datos de la película --}}
+</div>
+</div>
+</body>
+</html>
+
+
+
