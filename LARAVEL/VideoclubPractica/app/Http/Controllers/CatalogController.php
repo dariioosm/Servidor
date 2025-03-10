ddee5445201467c0dfdf1780fdc0cf8f10e02c68
Dario Sanchez <dariosanchez351@gmail.com>
2025-03-10 14:07:49 +0100

    Refactor CatalogController to improve code readability

--- LARAVEL/VideoclubPractica/app/Http/Controllers/CatalogController.php
+++ LARAVEL/VideoclubPractica/app/Http/Controllers/CatalogController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class CatalogController extends Controller
 {
-    
+    //? ::all() coge todos los campos del array asociativo que se ha seleccionado
 
     public function getIndex(){
         return view('catalog.index',[array('arrayPeliculas')=>Pelicula::all()]);
