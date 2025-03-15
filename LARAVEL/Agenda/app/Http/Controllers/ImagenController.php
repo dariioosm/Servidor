00c5baf398b4a6d7c0112750fbb5acae12f2d6ba
dariioosm <dariosanchez351@gmail.com>
2025-03-15 07:08:25 +0100

    Agregar método 'store' en 'ImagenController' para guardar nuevas imágenes con validación de campos

--- LARAVEL/Agenda/app/Http/Controllers/ImagenController.php
+++ LARAVEL/Agenda/app/Http/Controllers/ImagenController.php
@@ -10,5 +10,16 @@ class ImagenController extends Controller
         $imagenes = Imagen::all();
         return view('imagen.index',compact('imagenes'));
     }
+public function store(Request $request){
+    if(!empty($request->categoria)&&!empty($request->imgaen)&&!empty($request->descripcion)){
+        $i = new Imagen;
+        $i->categoria=$request->post('idimagen');
+        $i->imagen=$request->post('categoria');
+        $i->imagen=$request->post('descripcion');
+        $i->save();
+        return redirect('imagen.index');
+
+    }
+}
 
 }
