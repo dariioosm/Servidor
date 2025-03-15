c154c755d1c078b8a168a697d9eb88e2927c48f2
dariioosm <dariosanchez351@gmail.com>
2025-03-15 02:35:09 +0100

    Actualizar el seeder de 'Agenda' para insertar múltiples registros con timestamps

--- LARAVEL/Agenda/database/seeders/AgendaTableSeeder.php
+++ LARAVEL/Agenda/database/seeders/AgendaTableSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Agenda;
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
 class AgendaTableSeeder extends Seeder
 {
     /**
@@ -13,9 +13,11 @@ class AgendaTableSeeder extends Seeder
      */
     public function run(): void
     {
-
-        Agenda::create(['fecha'=>'2025-01-20','hora'=>'09:00:00','idpersona'=>1,'idimagen'=>3]);
-        Agenda::create(['fecha'=>'2025-01-20','hora'=>'21:00:00','idpersona'=>1,'idimagen'=>5]);
-        Agenda::create(['fecha'=>'2025-01-20','hora'=>'13:00:00','idpersona'=>2,'idimagen'=>7]);
-    }
+        DB::table('agenda')->insert([
+         ['id' => 2, 'fecha' => '2025-01-20', 'hora' => '09:00:00', 'idpersona' => 1, 'idimagen' => 3, 'created_at' => now(), 'updated_at' => now()],
+         ['id' => 3, 'fecha' => '2025-01-20', 'hora' => '21:00:00', 'idpersona' => 1, 'idimagen' => 5, 'created_at' => now(), 'updated_at' => now()],
+         ['id' => 4, 'fecha' => '2025-01-20', 'hora' => '13:00:00', 'idpersona' => 2, 'idimagen' => 7, 'created_at' => now(), 'updated_at' => now()],
+         ['id' => 5, 'fecha' => '2025-01-20', 'hora' => '18:00:00', 'idpersona' => 1, 'idimagen' => 6, 'created_at' => now(), 'updated_at' => now()],
+        ]);
+     }
 }
