4d972d5a855da3889b94a3ae12cd739c185811e7
dariioosm <dariosanchez351@gmail.com>
2025-03-15 01:17:44 +0100

    Actualizar configuración de base de datos y agregar migraciones y seeders para las tablas 'imagenes', 'personas' y 'agenda'

---
+++ LARAVEL/Agenda/database/seeders/AgendaTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AgendaTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
---
+++ LARAVEL/Agenda/database/seeders/ImagenesTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ImagenesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
