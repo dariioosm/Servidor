1353c0b6c1dc91c05b7e9ca617ce3c3ebe1da788
Dario Sanchez <dariosanchez351@gmail.com>
2025-04-07 12:46:48 +0200

    .

--- EJERCICIOS DE RECUPERACION/Simon/jugar.php
+++ EJERCICIOS DE RECUPERACION/Simon/jugar.php
@@ -7,14 +7,7 @@
 </head>
 <body>
 <?php
-session_start();
-function dibujar_circulos($pinta){
-    for($i=0;$i<4;$i++){
-        echo'<svg width="100" height="100">';
-        echo'<circle id="circulo'.($i+1).'" cx="50" cy="50" r="40" fill="'.$pinta[$i].'"></circle>';
-        echo'</svg>';
-    }
-}
+
 //TODO hacer el contador
 if(!isset($_SESSION['contador'])){
     $_SESSION['contador']=0;
