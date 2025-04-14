6b46d8d225b3cf99852a5da061f74eddd2f8425c
Dario Sanchez <dariosanchez351@gmail.com>
2025-04-14 14:18:55 +0200

    Fix variable reference for total attempts and streamline point updates in comprueba.php

--- EJERCICIOS DE RECUPERACION/Cartas/comprueba.php
+++ EJERCICIOS DE RECUPERACION/Cartas/comprueba.php
@@ -27,19 +27,40 @@ $extra->bind_param('s',$_SESSION['usuario']);
 $extra->execute();
 $resultadoextra=$extra->get_result();
 $filaextra = $resultadoextra->fetch_assoc();
-$totalintentos=intval($fila['extra']);
+$totalintentos=intval($filaextra['extra']);
 
 $nuevosintentos=$totalintentos+$_SESSION['levantadas'];
 
 //?update de la tupla con el nuevo resultado
 
-$update= $conn->prepare('UPDATE jugador SET extra = ? WHERE login = ?')
+$update= $conn->prepare('UPDATE jugador SET extra = ? WHERE login = ?');
+$update->bind_param('is',$intentos,$_SESSION['usuario']);
+$update->execute();
 
 
 if($respuestax==$respuestay){
-    
+    $puntos=$conn->prepare('SELECT puntos FROM jugador WHERE jugador = ?');
+    $puntos->bind_param('s',$_SESSION['usuario']);
+    $puntos->execute();
+    $resultadopuntos=$puntos->get_result();
+    $filapuntos = $resultadopuntos->fetch_assoc();
+    $totalpuntos=intval($filapuntos['puntos']);
+    $nuevospuntos = $totalpuntos+1;
+
+    $updatepuntos= $conn->prepare('UPDATE jugador SET puntos = ? WHERE login = ?');
+    $updatepuntos->bind_param('is',$nuevospuntos,$_SESSION['usuario']);
+    $updatepuntos->execute();
 }else{
+    $fallos=$conn->prepare('SELECT puntos FROM jugador WHERE jugador = ?');
+    $fallos->bind_param('s',$_SESSION['usuario']);
+    $fallos->execute();
+    $resultadofallos=$fallos->get_result();
+    $filafallos = $resultadofallos->fetch_assoc();
+    $totalfallos=intval($filafallos['puntos']);
+    $nuevosfallos = $totalfallos-1;
 
+    $updatefallos= $conn->prepare('UPDATE jugador SET puntos = ? WHERE login = ?');
+    $updatefallos->bind_param('is',$nuevosfallos,$_SESSION['usuario']);
+    $updatefallos->execute();
 }
-
 ?>
