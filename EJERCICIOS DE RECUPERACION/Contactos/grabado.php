2e467c943ae5a2248224eaadebbfd0c4fd02b583
Dario Sanchez <dariosanchez351@gmail.com>
2025-04-30 03:40:44 +0200

    Refactor contact insertion logic in agenda.php and update redirection links in grabado.php and totales.php

--- EJERCICIOS DE RECUPERACION/Contactos/grabado.php
+++ EJERCICIOS DE RECUPERACION/Contactos/grabado.php
@@ -13,7 +13,7 @@ session_start();
     <h1>Agenda</h1><br>
     <h2>Hola <?php echo htmlspecialchars($_SESSION['usuario']);?> </h2><br>
     <p>Se han grabado tus <?php echo htmlspecialchars($_SESSION['incrementos'])?> contactos :D</p><br>
-    <a href="login.php">Volver a loguearse</a> <br>
+    <a href="index.php"><?php session_destroy(); ?>Volver a loguearse</a> <br>
     <a href="agenda.php">Agregar mas contactos para <?php unset($_SESSION['incrementos']); echo htmlspecialchars($_SESSION['usuario']);?></a><br>
     <a href="totales.php">Total de contactos guardados</a>
 
